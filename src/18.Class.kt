41b2ed6447dcd0ac654f3ff0151a158da40efc73
kimyeonghwi <pwrkim12@gmail.com>
2021-07-08 17:25:20 +0900

    SQL

--- src/18.Class.kt
+++ src/18.Class.kt
@@ -12,6 +12,105 @@
 // 축구게임 -> 필요한 객체 선수 , 심판 ,축구장 ,축구공, 관중 ,골대  등등등등등 모든것이다 객체이다.
 //이러한 객체들이 모여서 서로 서로 협력하며 프로그래밍 되는게 객체지향프로그램
 
+
+
+
+
 fun main() {
 
+
+    // 인스턴스화 하기
+    val bigCar =  Car("a8 engine", " big")
+
+    val bigCar1 : Car = Car("a9 engine" , "big" )
+
+    val superCar : SuperCar = SuperCar("z engine" , "big" , "wing")
+
+    val runCar : RunCar = RunCar("구린앤진" , "구린바디 ")
+
+    runCar.ride()
+    runCar.navi("전주")
+    runCar.run()
+
 }
+
+
+// 클래스 만드는 방법 (1)
+class Car(var engine : String , var body : String){
+
+}
+
+// 클래스 만드는 방법 (2) 자바랑 똑같음
+class SuperCar {
+    var engine : String =""
+    var body : String = ""
+    var door : String = ""
+
+    constructor(engine: String , body: String , door : String){
+        this.engine = engine
+        this.body = body
+        this.door = door
+    }
+
+}
+
+//클래스 만드는 방법 (3) -> 1번 방법의 확장  ======> 꼭 필요한것만 먼저 받을때 이렇게 사용한다.
+class Car1 constructor(engine : String , body : String) {
+//위 constructor 안써도 된다!!!
+
+   var door : String = ""                                       // 이부분에 this는 꼭 필요한것 적어 주면 된다 .
+    constructor( engine: String , body: String , door: String) : this (engine, body){
+        //이곳은 선택사항
+        this.door = door
+    }
+
+}
+
+//클래스 만드는 방법 (4)  2번 방법의 확장
+class Car2 {
+    var engine : String = ""
+    var body : String = ""
+    var door : String = ""
+    //오버로딩
+    constructor(engine: String , body: String){
+        this.body = body
+        this.engine = engine
+    }
+    //오버로딩
+    constructor(engine: String , body: String , door: String){
+        this.engine = engine
+        this.body = body
+        this.door = door
+    }
+}
+
+class RunCar(engine: String , body: String){
+
+    // 객체를 만들면 무조건 출력이 되는 것이다. ★★★★★
+    init {
+        //초기셋팅 할 때 유용하다.
+        println("런카 만들었음 ㅋㅋㅋ")
+    }
+
+   fun ride(){
+       println("탑승했음")
+   }
+    fun run(){
+        println("달립니다")
+    }
+    fun navi (point : String){
+        println("$point (으)로 목적지설정 되었습니다.")
+    }
+
+}
+
+//오버로딩 매개변수가 다른 같은 이름의 메소드
+class TestClass() {
+
+    fun a (a : String ){
+    }
+    fun b (b : Int){
+    }
+}
+
+
---
+++ src/19.ClassQuiz.kt
@@ -0,0 +1,99 @@
+
+
+fun main(){
+
+    val math = Math()
+    println(math.aaa(1 ,2)) // 3
+    println(math.bbb(5 ,2)) // 3
+    println(math.ccc(2 ,2)) // 4
+    println(math.ddd(10 ,2)) // 5
+
+
+    println()
+
+    val bank = Bank("아옳이 계좌 " , "1991-05-23")
+    bank.add(1000)
+    bank.minus(500)
+    bank.showMoney()
+
+    println()
+    val tv = Tv (listOf("K","M","S"))
+    tv.turn()
+    println(tv.onOrOff)
+    tv.turn()
+    println(tv.onOrOff)
+    tv.turn()
+    println(tv.onOrOff)
+    tv.changeup()
+    tv.changeup()
+    tv.changeup()
+    tv.changeup()
+    tv.changeup()
+    tv.changeup()
+
+}
+
+class Math(){
+
+    fun aaa(a : Int , b : Int ): Int{
+        return a+b
+    }
+    fun bbb(a : Int , b : Int ): Int{
+        return a-b
+    }
+    fun ccc(a : Int , b : Int ): Int{
+        return a*b
+    }
+    fun ddd(a : Int , b : Int ): Int{
+        return a/b
+    }
+
+}
+
+
+class Bank(name : String , birth : String){
+
+    var money : Int = 0
+
+    init {
+        println("$name 님의 계좌가 생성되었습니다.")
+    }
+    fun showMoney(){
+        println("$money 만큼 돈있어용")
+    }
+    fun add(a : Int){
+        money+= a
+        println("$a 만큼 입금했음")
+    }
+    fun minus(a : Int){
+        money-= a
+        println("$a 만큼 출금했음")
+    }
+}
+
+class Tv (val chnel : List<String>){
+
+    var onOrOff : Boolean = false
+    var current = 0
+        set(value) {
+            field = value
+            if(field >2){
+                field = 0
+            }
+        }
+
+
+    fun turn (){
+       onOrOff = !onOrOff
+    }
+
+    fun check():String {
+        return chnel[current]
+    }
+    fun changeup () {
+        current = current +1
+    }
+    fun changdown () {
+        current = current +1
+    }
+}
